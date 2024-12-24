12692ecba5bb58fe75946abaabdc3191fd15beb3
Ren Yamazaki <r26763041@gmail.com>
2024-12-25 05:13:37 +0900

    12/25 ログイン機能の実装と、homeページのviewの改善

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,6 +14,20 @@
             <a href="{{ route('factor-game') }}" class="game-button">因数分解ゲームを始める</a>
             <a href="#" class="game-button">他のゲームを追加</a>
         </div>
+        <div class="auth-links">
+            @guest
+                {{-- 未ログインの場合 --}}
+                <a href="{{ route('login') }}" class="auth-link">ログイン</a>
+                <a href="{{ route('register') }}" class="auth-link">新規登録</a>
+            @else
+                {{-- ログイン済みの場合 --}}
+                <span class="user-name">{{ Auth::user()->name }}さん</span>
+                <form method="POST" action="{{ route('logout') }}" class="logout-form">
+                    @csrf
+                    <button type="submit" class="logout-button">ログアウト</button>
+                </form>
+            @endguest
+        </div>
     </div>
 </body>
 </html>
